349caf97029b87b6347d7f7d527557a95c6b4956
Justin Nguyen <justinn123123123@gmail.com>
2023-07-04 17:50:13 -0400

    fixed navbar showing up weirdly on screen resize

--- website/script.js
+++ website/script.js
@@ -1,9 +1,18 @@
 var navLinks = document.getElementById("navLinks")
 
+window.addEventListener("resize", fixMenu);
+
 function showMenu(){
     navLinks.style.right = "0";
 }
 
 function hideMenu(){
     navLinks.style.right = "-400px";
+}
+
+function fixMenu(){
+    var w = window.innerWidth;
+    if(w>=1612){
+        hideMenu();
+    }
 }
