f393c490e8e0c443ed95fa6600c1c8820e4f3db0
Brennen Le <107582405+brthhule@users.noreply.github.com>
2023-07-21 21:52:44 -0400

    Synced with Justin branch

---
+++ script.js
@@ -0,0 +1,35 @@
+var navLinks = document.getElementById("navLinks")
+var links = document.querySelector(".js-being-wack")
+
+window.addEventListener("resize", function(){
+    var width = window.innerWidth;
+    if(width>=1612){
+        hideMenu();
+    }
+});
+
+var ignore = document.querySelector("#burger-icon")
+
+window.addEventListener("click",function(event){
+    if(event.target !== navLinks && 
+        event.target.parentNode !== navLinks && 
+        event.target !== ignore && 
+        event.target.parentNode !== ignore &&
+        event.target.parentNode !== links){
+        hideMenu();
+    }
+})
+
+
+function showMenu(){
+    navLinks.style.right = "0";
+}
+
+function hideMenu(){
+    navLinks.style.right = "-500px";
+}
+
+function datalistcall() {
+    var o1 = document.getElementById("carsInput").value;
+    document.getElementById("output").innerHTML = "You select " + o1 + " option";
+}
